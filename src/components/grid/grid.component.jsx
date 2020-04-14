662b5488d959d2f668c8f90794a5b466a72668f7
Sumit <ghewdesumit@gmail.com>
2020-04-14 09:56:13 -0400

    developed a react tic tac toe game

---
+++ src/components/grid/grid.component.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import "./grid.styles.scss";
+import { stringToNumber } from "../../utils/grid.utils";
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentMarker: "X", message: null };
+    this.grid = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    this.count = 0;
+  }
+
+  checkequals = (a, b, c) => {
+    return a === b && b === c && a !== "";
+  };
+
+  handleClick = (event) => {
+    const { currentMarker } = this.state;
+
+    let [x, y] = stringToNumber(event.target.id);
+
+    this.grid[x][y] = currentMarker;
+
+    event.target.textContent = this.grid[x][y];
+    event.target.style.pointerEvents = "none";
+
+    if (currentMarker === "X") {
+      this.setState({ currentMarker: "O" });
+    } else {
+      this.setState({ currentMarker: "X" });
+    }
+    this.count++;
+    this.checkWinner();
+  };
+
+  disableBoxes = () => {
+    const boxElement = document.querySelectorAll(".box");
+    boxElement.forEach((item) => (item.style.pointerEvents = "none"));
+  };
+
+  checkWinner = () => {
+    if (this.count === 9) {
+      this.setState({
+        ...this.state,
+        message: `its a Tie!!! no one won the game please try again`,
+      });
+      this.disableBoxes();
+      return;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      //horizontal
+      if (this.checkequals(this.grid[i][0], this.grid[i][1], this.grid[i][2])) {
+        this.setState({
+          ...this.state,
+          message: `Winner of the Tic Tac Toe is ${this.grid[i][0]}`,
+        });
+        this.disableBoxes();
+        return;
+      }
+
+      //vertical
+      if (this.checkequals(this.grid[0][i], this.grid[1][i], this.grid[2][i])) {
+        this.setState({
+          ...this.state,
+          message: `Winner of the Tic Tac Toe is ${this.grid[0][i]}`,
+        });
+        this.disableBoxes();
+        return;
+      }
+    }
+
+    //diagonal from left to right
+    if (this.checkequals(this.grid[0][0], this.grid[1][1], this.grid[2][2])) {
+      this.setState({
+        ...this.state,
+        message: `Winner of the Tic Tac Toe is ${this.grid[0][0]}`,
+      });
+      this.disableBoxes();
+      return;
+    }
+
+    //diagonal from right to left
+    else if (
+      this.checkequals(this.grid[0][2], this.grid[1][1], this.grid[2][0])
+    ) {
+      this.setState({
+        ...this.state,
+        message: `Winner of the Tic Tac Toe is ${this.grid[0][2]}`,
+      });
+      this.disableBoxes();
+      return;
+    }
+  };
+
+  handleRefresh = () => {
+    window.location.reload();
+  };
+
+  render() {
+    let boxHolder = [];
+    let boxId = "";
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        boxId = `${i}${j}`;
+        boxHolder.push(
+          <div
+            key={boxId}
+            onClick={this.handleClick}
+            className="box"
+            id={boxId}
+          ></div>
+        );
+      }
+    }
+    return (
+      <div className="grid">
+        <div className="grid-container">{boxHolder}</div>
+        {this.state.message ? (
+          <div className="message-container">
+            <div className="message">{this.state.message}</div>
+            <button
+              onClick={this.handleRefresh}
+              className="play-again"
+              style={{ visibility: "visible" }}
+            >
+              Play Again
+            </button>
+          </div>
+        ) : (
+          <div className="message-container">
+            <div className="message">{`Your Turn player ${this.state.currentMarker}`}</div>
+            <button
+              onClick={this.handleRefresh}
+              className="play-again"
+              style={{ visibility: "hidden" }}
+            >
+              Play Again
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Grid;
---
+++ src/utils/grid.utils.js
@@ -0,0 +1,4 @@
+export const stringToNumber = (id) => {
+  let [x, y] = [parseInt(id[0]), parseInt(id[1])];
+  return [x, y];
+};
